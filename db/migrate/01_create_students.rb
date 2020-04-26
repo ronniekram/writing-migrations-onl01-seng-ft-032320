121f12f6c820df4b4bc2b3d1dbfe7c59923baf26
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-26 21:29:58 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change 
-    create_table (:students) {|s| s.column
+    create_table (:students) {|s| s.column :name, :string}
   end 
 end
