67b04621731fafd45b2f06412300f2c5dd54202c
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-26 21:32:49 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column 
+    add_column(:students, :birthdate, :string)
+    add_column(:students, :grade, :integer)
   end 
 
 end 
